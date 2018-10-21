302bed57adeebf24f7956c2f0df5fb9243b7df7e
as16afm <as16afm@cl-2se-009.herts.ac.uk>
2018-10-21 13:40:14 +0000

    Inserting a view image via Interface Builder

--- ViewImage_Exercise/ViewController.swift
+++ ViewImage_Exercise/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+//        //Create a new UIImageView from scratch
+//        var carView = UIImageView(image: nil)
+//
+//        //Assign an image to the image view
+//        carView.image = UIImage(named: "car1.png")
+//
+//        //Assign the size and position of the image view
+//        carView.frame = CGRect(x:100, y:100, width: 30, height: 50)
+//
+//        //Add the image view to the main view
+//        self.view.addSubview(carView)
     }
 
     override func didReceiveMemoryWarning() {
