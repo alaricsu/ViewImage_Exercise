6c4b806d9e072789cee32cf20b54f41e1208b87a
as16afm <as16afm@cl-2se-009.herts.ac.uk>
2018-10-21 13:17:19 +0000

    Initial Commit

---
+++ ViewImage_ExerciseTests/ViewImage_ExerciseTests.swift
@@ -0,0 +1,36 @@
+//
+//  ViewImage_ExerciseTests.swift
+//  ViewImage_ExerciseTests
+//
+//  Created by as16afm on 21/10/2018.
+//  Copyright © 2018 as16afm. All rights reserved.
+//
+
+import XCTest
+@testable import ViewImage_Exercise
+
+class ViewImage_ExerciseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
